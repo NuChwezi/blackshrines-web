860a30220d5858c74a22bd77bba8d54c2ad2fd2a
Nemesis Fixx <joewillrich@gmail.com>
2016-11-08 18:36:26 +0300

    With love...

--- js/elite/namon.js
+++ js/elite/namon.js
@@ -1,9 +1,16 @@
 $(document).ready(function() {
     SOUND_RES['GODS']['NAMON'] = [
         'sounds/music/lucifer/Vangelis - chariots of fire.mp3',
+        'sounds/music/namon/Brian Eno - An Ending.mp3',
     ];
     mantras['NAMON'] = [
         "DEATH TO THE WEAKLING, WEALTH TO THE STRONG",
+        "NEVER GIVE SOMETHING FOR NOTHING, NEVER!",
+        "LOVE MONEY, AND I WILL LOVE YOU BACK",
+        "INVEST! DON'T WASTE!",
+        "WHY NOT SAVE IF YOU CAN?",
+        "DON'T WASTE TIME!",
+        "I BLESS YOU. GO ON, BE WEALTHY!",
     ];
 
     $('#gods-list').prepend('<li><a href="#">NAMON</a></li>');
